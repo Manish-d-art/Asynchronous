445ba83df7132b7236b6625bb56b2c0f71ff2cbc
manish mandal <76776194mm@gmail.com>
2022-10-01 10:21:22 +0530

    update script.js

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+
+const request=new XMLHttpRequest();
+request.open('GET','https://restcountries.com/v3.1/name/india');
+request.send();
+
+
