36231e83d1353bc48752848bbec43c5aff18023a
manish mandal <76776194mm@gmail.com>
2022-10-01 11:13:41 +0530

    update script.js

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ request.addEventListener('load',function(){
   <article class="country">
           <img class="country__img" src="${data.flags.png}" />
           <div class="country__data">
-            <h3 class="country__name">${data.name}</h3>
+            <h3 class="country__name">${data.name.comman}</h3>
             <h4 class="country__region">${data.region}</h4>
             <p class="country__row"><span>👫</span>${(+data.population/1000000).toFixed(1)} people</p>
             <p class="country__row"><span></span>${data.languages[0]}</p>
