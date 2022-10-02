81f94c45f0376a95c34f46f35f868c5e22c20afb
manish mandal <76776194mm@gmail.com>
2022-10-02 14:05:56 +0530

    update script.js

--- script.js
+++ script.js
@@ -5,29 +5,34 @@ const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
 
-const request=new XMLHttpRequest();
-request.open('GET','https://restcountries.com/v3.1/name/bharat');
-request.send();
+const getCountryData=function(country){
 
-request.addEventListener('load',function(){
-  const [data]=JSON.parse(this.responseText);
-  console.log(data);
-  const html=`
+    const request=new XMLHttpRequest();
+    request.open('GET',`https://restcountries.com/v3.1/name/${country}`);
+    request.send();
 
-  <article class="country">
-          <img class="country__img" src="${data.flags.png}" />
-          <div class="country__data">
-            <h3 class="country__name">${data.name.comman}</h3>
-            <h4 class="country__region">${data.region}</h4>
-            <p class="country__row"><span>👫</span>${(+data.population/1000000).toFixed(1)} people</p>
-            <p class="country__row"><span></span>${data.languages[0]}</p>
-            <p class="country__row"><span></span>${data.currencies.INR[0]}</p>
-          </div>
-        </article>
-  `;
+    request.addEventListener('load',function(){
+      const [data]=JSON.parse(this.responseText);
+      console.log(data);
+      const html=`
 
-  countriesContainer.insertAdjacentHTML('beforeend',html);
-  countriesContainer.style.opacity="1";
-});
+      <article class="country">
+              <img class="country__img" src="${data.flags.png}" />
+              <div class="country__data">
+                <h3 class="country__name">${data.name.comman}</h3>
+                <h4 class="country__region">${data.region}</h4>
+                <p class="country__row"><span>👫</span>${(+data.population/1000000).toFixed(1)} people</p>
+                <p class="country__row"><span></span>${data.languages[0]}</p>
+                <p class="country__row"><span></span>${data.currencies.name}</p>
+              </div>
+            </article>
+      `;
 
+      countriesContainer.insertAdjacentHTML('beforeend',html);
+      countriesContainer.style.opacity="1";
+    });
+  }
+
+  getCountryData('bharat');
+  getCountryData('portugal'); 
 
