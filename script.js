8db07675fb0d3f11c8b068b1d0f17f6876c482dc
manish mandal <76776194mm@gmail.com>
2022-10-11 00:07:22 +0530

    update script.js

--- script.js
+++ script.js
@@ -271,4 +271,54 @@ const whereAmI = function(){
     .finally(() => (countriesContainer.style.opacity = 1));
 
 };
-whereAmI();
+// whereAmI();
+
+
+// coding challenge2
+const imgContainer=document.querySelector('.images');
+
+const createImage=function(imgPath){
+  return new Promise(function(resolve,reject){
+    const img=document.createElement('img');
+    img.src=imgPath;
+
+    img.addEventListener('load',function(){
+      imgContainer.append(img);
+      resolve(img);
+    });
+
+    img.addEventListener('error',function(){
+      reject(new Error('Image not found'))
+    })
+  });
+};
+
+const wait = function(seconds) {
+    return new Promise(function(resolve) {
+        setTimeout(resolve, seconds * 1000);
+    });
+};
+
+let currImg;
+createImage('./img/img-1.jpg')
+    .then(img => {
+        currImg = img;
+        console.log('img1 loaded');
+        return wait(2);
+    })
+    .then(() => {
+        currImg.style.display = 'none';
+        return createImage('img/img-2.jpg');
+    })
+    .then(img => {
+        currImg = img;
+        console.log('img2 loaded');
+        return wait(2);
+    })
+    .then(() => {
+        currImg.style.display = 'none';
+        console.log('final display');
+        return wait(2);
+    })
+    .then(() => (currImg.style.display = 'block'))
+    .catch(err => console.error(err));
